e1e5d770c97f4ec38dc2ba35523f592c75e57840
aoken7 <aokensabu1@gmail.com>
2021-07-11 22:33:29 +0900

    add Program.cs

---
+++ LifeGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LifeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var automaton = new Automaton(16,32);
+
+            while (true)
+            {
+                automaton.Print();
+                System.Threading.Thread.Sleep(1500);
+                automaton.Update();
+            }
+        }
+    }
+}
